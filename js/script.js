6482feb1b047f32b199da28854678479c7331082
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-25 21:40:29 +0530

    feat(core): implement main functionality with JS
    Added core JavaScript logic to handle the primary functionality of the project. This includes:
    - Creating a Book object constructor
    - Creating a addBookToLibrary() function to add users' books
    - Creating a displayBooks() function to display users' books in a table
    - Adding a button to allow the user to add new books via dialogs and modals
    - Adding a delete button to allow the user to remove a book from the table
    To-Do: Add a button on each book’s display to change its read status.

---
+++ js/script.js
@@ -0,0 +1,133 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
+
+
+function displayBooks() {
+    /*
+    Here, I am essentially clearing all the existing table rows and then recreating it from scratch.
+    I might tackle this in a better way.
+    */
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
+    }
+
+    // Clear header row
+    const headerRow = document.createElement("tr");
+    const titleHeader = document.createElement("th");
+    const authorHeader = document.createElement("th");
+    const pagesHeader = document.createElement("th");
+    const readStatusHeader = document.createElement("th");
+    
+    titleHeader.textContent = "Title";
+    authorHeader.textContent = "Author";
+    pagesHeader.textContent = "Pages";
+    readStatusHeader.textContent = "Read Status";
+    //readStatusHeader.rowSpan = "2";
+    headerRow.appendChild(titleHeader);
+    headerRow.appendChild(authorHeader);
+    headerRow.appendChild(pagesHeader);
+    headerRow.appendChild(readStatusHeader);
+    
+    table.appendChild(headerRow);
+    
+    for (i = 0; i < myLibrary.length; i++) {
+        const bookRow = document.createElement("tr");
+
+        const titleCell = document.createElement("td");
+        const authorCell = document.createElement("td");
+        const pagesCell = document.createElement("td");
+        const readStatusCell = document.createElement("td");
+        const deleteBookCell = document.createElement("td");
+
+        titleCell.textContent = myLibrary[i].title;
+        authorCell.textContent = myLibrary[i].author;
+        pagesCell.textContent = myLibrary[i].pages;
+        readStatusCell.textContent = myLibrary[i].read;
+
+        const deleteBookButton = document.createElement("button")
+        deleteBookButton.textContent = "Delete";
+        deleteBookButton.addEventListener("click", () => {
+            myLibrary.pop(myLibrary[i]);
+            displayBooks();
+        })
+
+        deleteBookCell.appendChild(deleteBookButton);
+
+        bookRow.appendChild(titleCell);
+        bookRow.appendChild(authorCell);
+        bookRow.appendChild(pagesCell);
+        bookRow.appendChild(readStatusCell);
+        bookRow.appendChild(deleteBookCell);
+
+        table.appendChild(bookRow);
+    }
+};
+
+
+const addNewBookButton = document.querySelector("#add");
+const dialog = document.querySelector("dialog");
+const closeDialogButton = document.querySelector("dialog button");
+const submitButton = document.querySelector("#sub");
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+
+const table = document.querySelector("table");
+
+const headerRow = document.createElement("tr");
+const titleHeader = document.createElement("th");
+const authorHeader = document.createElement("th");
+const pagesHeader = document.createElement("th");
+const readStatusHeader = document.createElement("th");
+
+titleHeader.textContent = "Title";
+authorHeader.textContent = "Author";
+pagesHeader.textContent = "Pages";
+readStatusHeader.textContent = "Read Status";
+//readStatusHeader.rowSpan = "2";
+headerRow.appendChild(titleHeader);
+headerRow.appendChild(authorHeader);
+headerRow.appendChild(pagesHeader);
+headerRow.appendChild(readStatusHeader);
+
+table.appendChild(headerRow);
+
+
+// "Show the dialog" button opens the dialog modally
+
+addNewBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeDialogButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+submitButton.addEventListener("click", () => {
+    const book = new Book(title.value, author.value, pages.value, yes.checked ? "yes" : "no");
+    addBookToLibrary(book);
+    displayBooks();
+    dialog.close();
+
+});
+
+
