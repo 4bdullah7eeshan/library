c2b089459ed4e34ff724ff579d04ba03f91d769f
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-26 00:23:22 +0530

    feat(layout): setup page layout

--- js/script.js
+++ js/script.js
@@ -32,49 +32,65 @@ function displayBooks() {
     const authorHeader = document.createElement("th");
     const pagesHeader = document.createElement("th");
     const readStatusHeader = document.createElement("th");
+    const manageHeader = document.createElement("th");
     
     titleHeader.textContent = "Title";
     authorHeader.textContent = "Author";
     pagesHeader.textContent = "Pages";
     readStatusHeader.textContent = "Read Status";
-    //readStatusHeader.rowSpan = "2";
+    manageHeader.textContent = "Manage"
+
     headerRow.appendChild(titleHeader);
     headerRow.appendChild(authorHeader);
     headerRow.appendChild(pagesHeader);
     headerRow.appendChild(readStatusHeader);
+    headerRow.appendChild(manageHeader);
     
     table.appendChild(headerRow);
-    
-    for (i = 0; i < myLibrary.length; i++) {
-        const bookRow = document.createElement("tr");
 
-        const titleCell = document.createElement("td");
-        const authorCell = document.createElement("td");
-        const pagesCell = document.createElement("td");
-        const readStatusCell = document.createElement("td");
-        const deleteBookCell = document.createElement("td");
+    if (myLibrary.length === 0) {
+        const emptyRow = document.createElement("tr");
+        const emptyCell = document.createElement("td");
 
-        titleCell.textContent = myLibrary[i].title;
-        authorCell.textContent = myLibrary[i].author;
-        pagesCell.textContent = myLibrary[i].pages;
-        readStatusCell.textContent = myLibrary[i].read;
+        // Span all columns
+        emptyCell.colSpan = "5";  // Adjust colSpan based on the number of columns
+        emptyCell.classList.add("empty");
+        emptyCell.textContent = "Currently there are no books in your library!";
+        emptyRow.appendChild(emptyCell);
+        table.appendChild(emptyRow);
+    } else {
+        for (i = 0; i < myLibrary.length; i++) {
+            const bookRow = document.createElement("tr");
 
-        const deleteBookButton = document.createElement("button")
-        deleteBookButton.textContent = "Delete";
-        deleteBookButton.addEventListener("click", () => {
-            myLibrary.pop(myLibrary[i]);
-            displayBooks();
-        })
+            const titleCell = document.createElement("td");
+            const authorCell = document.createElement("td");
+            const pagesCell = document.createElement("td");
+            const readStatusCell = document.createElement("td");
+            const manageBookCell = document.createElement("td");
 
-        deleteBookCell.appendChild(deleteBookButton);
+            titleCell.textContent = myLibrary[i].title;
+            authorCell.textContent = myLibrary[i].author;
+            pagesCell.textContent = myLibrary[i].pages;
+            readStatusCell.textContent = myLibrary[i].read;
 
-        bookRow.appendChild(titleCell);
-        bookRow.appendChild(authorCell);
-        bookRow.appendChild(pagesCell);
-        bookRow.appendChild(readStatusCell);
-        bookRow.appendChild(deleteBookCell);
+            const deleteBookButton = document.createElement("button")
+            //deleteBookButton.textContent = "Delete";
+            deleteBookButton.innerHTML = '<i class="mdi mdi-delete"></i>'; // MDI delete icon
+            deleteBookButton.addEventListener("click", () => {
+                myLibrary.pop(myLibrary[i]);
+                displayBooks();
+            })
 
-        table.appendChild(bookRow);
+            manageBookCell.appendChild(deleteBookButton);
+
+            bookRow.appendChild(titleCell);
+            bookRow.appendChild(authorCell);
+            bookRow.appendChild(pagesCell);
+            bookRow.appendChild(readStatusCell);
+            bookRow.appendChild(manageBookCell);
+
+            table.appendChild(bookRow);
+        }
     }
 };
 
@@ -97,18 +113,32 @@ const titleHeader = document.createElement("th");
 const authorHeader = document.createElement("th");
 const pagesHeader = document.createElement("th");
 const readStatusHeader = document.createElement("th");
+const manageHeader = document.createElement("th");
+
 
 titleHeader.textContent = "Title";
 authorHeader.textContent = "Author";
 pagesHeader.textContent = "Pages";
 readStatusHeader.textContent = "Read Status";
-//readStatusHeader.rowSpan = "2";
+manageHeader.textContent = "Manage";
+
 headerRow.appendChild(titleHeader);
 headerRow.appendChild(authorHeader);
 headerRow.appendChild(pagesHeader);
 headerRow.appendChild(readStatusHeader);
+headerRow.appendChild(manageHeader);
+
+const emptyRow = document.createElement("tr");
+const emptyCell = document.createElement("td");
+
+        // Span all columns
+emptyCell.colSpan = "5";  // Adjust colSpan based on the number of columns
+emptyCell.classList.add("empty");
+emptyCell.textContent = "Currently there are no books in your library!";
+emptyRow.appendChild(emptyCell);
 
 table.appendChild(headerRow);
+table.appendChild(emptyRow);
 
 
 // "Show the dialog" button opens the dialog modally
